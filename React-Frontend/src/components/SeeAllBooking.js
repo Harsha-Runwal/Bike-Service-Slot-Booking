05e9354aef8ac9d3a2eb827eaac14fec2d26cd6a
harsha runwal <22runwalharsha33333@gmail.com>
2022-09-19 18:35:49 +0530

    Addplan and seeAllBookings componenet added

--- React-Frontend/src/components/SeeAllBooking.js
+++ React-Frontend/src/components/SeeAllBooking.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from "react-router-dom";
 import Axios from 'axios';
 import Logout from './authenticate/Logout';
@@ -6,22 +6,49 @@ import Logout from './authenticate/Logout';
 
 export default function SeeAllBooking()
 {
+  const location = useLocation();
+
+   
+    let [arr,setarr] = useState(location.state.name)     //got array of objects containing booking objects
+
+      
+      const all_list = arr.map((e)=>{ return(
+       <tr> <td>{e.bookingDate}</td>
+        <td>{e.bookingId}</td>
+        <td>{e.modelNo}</td>
+        <td>{e.planName}</td>
+        <td>{e.totalCost}</td>
+        <td>{e.vehicleRegNo}</td></tr>)
+      }) 
+     
     return(
         <div>
+           
             <h3>All Bookings for Today</h3>
-             {/**<h5> Welcome {location.state.name} </h5>  */}
+<div>
+              
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Booking Date </th>
+        <th>Booking Id </th>
+        <th>Model No </th>
+        <th>Plan Name </th>
+        <th>Total cost </th>
+        <th>vehicle Reg No </th>
+      </tr>
+    </thead>
+    <tbody>
+      
+       {all_list}
+      
+       
+    </tbody>
+  </table>
+</div>
 
-
-
-
-
-
-
-
-            {/**Logout button to destroy jwt token and navigate to home page*/}
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-            <button class="btn btn-danger" type="button" onClick={Logout}>Logout</button>
-        </div>
+            
+        
         </div>
     );
 }
